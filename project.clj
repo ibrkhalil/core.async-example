1219bf8cb60e3efd356ce9959e0dd868f6f4c4dd
Ibrahem Khalil <33176106+vampirekiddo@users.noreply.github.com>
2022-06-19 13:09:09 +0200

    Add http-kit and cheshire deps

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
-                 [org.clojure/core.async "1.5.648"]]
+                 [org.clojure/core.async "1.5.648"]
+                 [cheshire "5.11.0"]
+                 [http-kit "2.7.0-alpha1"]]
   :repl-options {:init-ns core.async-example})
